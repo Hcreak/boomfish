899eaf6d9ee6a1ef6c5f08acc4a4882b7c28b06a
Hcreak <hcreak@outlook.com>
2018-05-07 19:22:38 +0800

    toooooooooooooo Fix, fuck

--- boomfish.py
+++ boomfish.py
@@ -3,26 +3,24 @@ from flask import *
 import urllib, hashlib
 import time
 import sqlite3
-from os import urandom,path
+from os import urandom, path
+import json
 
 app = Flask(__name__)
 app.debug = True
 
 app.secret_key = urandom(16)
-
 randomkey = urandom(48)
+
 if path.exists('/boomfish/db/test.db'):
     DATABASE_URL = '/boomfish/db/test.db'
 else:
     DATABASE_URL = 'test.db'
 # DATABASE_URL=':memory:'
-admin_uesername = 'hcreak'
+
+admin_username = 'hcreak'
 admin_password = 'kotori'
 
-
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World!'
 
 def gravatar_url(email):
     # gravatar_url = "https://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
@@ -40,9 +38,45 @@ def checkkey():
     else:
         return False
 
-def getdata():
+
+# old getdata (ALL)
+# def getdata():
+#     conn = sqlite3.connect(DATABASE_URL)
+#     cur = conn.execute("SELECT * FROM data;")
+#     conn.commit()
+#
+#     itemlist = [{'id': item[0],
+#                  'author': item[1],
+#                  'gravatar': item[2],
+#                  'time': str(item[3]),
+#                  'strtime': str(time.strftime("%B %d, %Y at %H:%M", time.localtime(item[3]))),
+#                  'text': item[4],
+#                  'weburl': item[5]} for item in cur.fetchall()]
+#     # email = ''
+#     # gravatar_url = "https://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
+#     # gravatar_url += urllib.urlencode({'s': str(32), 'r': 'X', 'd': 'identicon'})
+#
+#     # item = {}
+#     # item['author'] = 'q'
+#     # item['gravatar'] = gravatar_url
+#     # item['time'] = str(time.time())
+#     # item['strtime'] = str(time.strftime("%B %d, %Y at %H:%M", time.localtime()))
+#     # item['text'] = 'fuck!'
+#     # item['weburl'] = 'http://yyq.cn'
+#     # itemlist = []
+#     # itemlist.append(item)
+#     # itemlist.append(item)
+#     # itemlist.append(item)
+#
+#     mode = None
+#     if checkkey():
+#         mode = 'admin'
+#     return render_template("insert.html",itemlist=itemlist, mode=mode, admin_username=admin_username)
+
+def getdata(datanum):
+    execstr = "SELECT * FROM data WHERE " + (' or '.join('id=' + str(i) for i in datanum)) + ";"
     conn = sqlite3.connect(DATABASE_URL)
-    cur = conn.execute("SELECT * FROM data;")
+    cur = conn.execute(execstr)
     conn.commit()
 
     itemlist = [{'id': item[0],
@@ -52,32 +86,15 @@ def getdata():
                  'strtime': str(time.strftime("%B %d, %Y at %H:%M", time.localtime(item[3]))),
                  'text': item[4],
                  'weburl': item[5]} for item in cur.fetchall()]
-    # email = ''
-    # gravatar_url = "https://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
-    # gravatar_url += urllib.urlencode({'s': str(32), 'r': 'X', 'd': 'identicon'})
-
-    # item = {}
-    # item['author'] = 'q'
-    # item['gravatar'] = gravatar_url
-    # item['time'] = str(time.time())
-    # item['strtime'] = str(time.strftime("%B %d, %Y at %H:%M", time.localtime()))
-    # item['text'] = 'fuck!'
-    # item['weburl'] = 'http://yyq.cn'
-    # itemlist = []
-    # itemlist.append(item)
-    # itemlist.append(item)
-    # itemlist.append(item)
-
-    sum = len(itemlist)
 
     mode = None
     if checkkey():
         mode = 'admin'
-    return render_template("insert.html", sum=sum, itemlist=itemlist, mode=mode)
+    return render_template("insert.html", itemlist=itemlist, mode=mode, admin_username=admin_username)
+
 
 @app.route('/', methods=['GET'])
 def test():
-
     return render_template("index.html")
 
 
@@ -96,22 +113,21 @@ def add():
     return redirect('/')
 
 
-@app.route('/bug', methods=['GET'])
+@app.route('/bug', methods=['GET', 'POST'])
 def bug():
-    if not session.get('key'):
-        return render_template("login.html")
-    else:
-        session.clear()
-        return redirect('/')
+    if request.method == 'POST':
+        if request.form['username'] == admin_username and request.form['password'] == admin_password:
+            session['key'] = randomkey
+            return redirect('/')
+        else:
+            return redirect('/bug')
 
-
-@app.route('/login', methods=['POST'])
-def login():
-    if request.form['username'] == admin_uesername and request.form['password'] == admin_password:
-        session['key'] = randomkey
-        return redirect('/')
-    else:
-        return redirect('/bug')
+    if request.method == 'GET':
+        if not session.get('key'):
+            return render_template("login.html")
+        else:
+            session.clear()
+            return redirect('/')
 
 
 @app.route('/delete/<id>', methods=['GET'])
@@ -124,10 +140,25 @@ def delete(id):
     else:
         return redirect('/bug')
 
+
 @app.route('/refurbish', methods=['POST'])
 def refurbish():
-    # data=request.form['number']
-    return getdata()
+    data = json.loads(request.form['data'])
+    blist = [int(i) for i in data]
+
+    conn = sqlite3.connect(DATABASE_URL)
+    cur = conn.execute("SELECT id FROM data;")
+    conn.commit()
+    alist = [i[0] for i in cur.fetchall()]
+
+    delnums = list(set(blist).difference(set(alist)))  # blist V alist X --> send del
+    addnums = list(set(alist).difference(set(blist)))  # alist V blist X --> send add
+
+    if len(addnums) != 0:
+        return getdata(addnums)
+    else:
+        return 'Norefurbish'
+
 
 if __name__ == '__main__':
     app.run()
--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,38 @@
 $(document).ready(function () {
-    startRequest()
+    startRequest();
     setInterval("startRequest()",3000);
 });
 
 function startRequest()
 {
+    var n = [];
+    $(".numcomment").each(function () {
+        n.push($(this).attr('numid'));
+    });
+    n = JSON.stringify(n);
+
     $.ajax({
         type:'post',
+        async: false,
         url:'/refurbish',
-        // data:{'number':$("#number")},
-        data:{},
+        data:{data:n},
         dataType:'html',
         success:function (datas) {
-            var d = document.getElementById('insert')
-            d.innerHTML = datas
+            if (datas != 'Norefurbish') {
+                var d = document.getElementById('insert');
+                d.innerHTML = d.innerHTML + datas;
+                var s = document.getElementById('number');
+                s.innerText = $(".numcomment").length;
+            }
         }
 
     })
 }
+
+// function  sendcommit() {
+//     $.ajax({
+//         type:'post',
+//         url:'/commit',
+//
+//     })
+// }
